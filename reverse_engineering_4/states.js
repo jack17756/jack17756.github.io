1c5ed988b1e821d9bcc87b1bb47700c494a04122
jack17756 <jack.liu4715@gmail.com>
2017-04-22 00:10:55 -0400

    Add files via upload

---
+++ reverse_engineering_4/states.js
@@ -0,0 +1,112 @@
+
+		$(document).ready(function() {
+
+			$(".state-groups").click(function(){$(".state-groups.active").removeClass("active"); $(this).addClass("active"); });
+
+			$(".nationwide-btn").click(function() {$(".card").hide(); $("#national").show(); $(".state-groups.active").removeClass("active");});
+
+			$("#NH").on("click", function() {$(".card").hide();$("#NH-card").show();});
+
+			$("#RI").on("click", function() {$(".card").hide();$("#RI-card").show();});
+
+			$("#ME").on("click", function() {$(".card").hide();$("#ME-card").show();});
+
+			$("#VT").on("click", function() {$(".card").hide();$("#VT-card").show();});
+
+			$("#MA").on("click", function() {$(".card").hide();$("#MA-card").show();});
+
+			$("#CT").on("click", function() {$(".card").hide();$("#CT-card").show();});
+
+			$("#DE").on("click", function() {$(".card").hide();$("#DE-card").show();});
+
+			$("#NY").on("click", function() {$(".card").hide();$("#NY-card").show();});
+
+			$("#NJ").on("click", function() {$(".card").hide();$("#NJ-card").show();});
+
+			$("#MD").on("click", function() {$(".card").hide();$("#MD-card").show();});
+
+			$("#DC").on("click", function() {$(".card").hide();$("#DC-card").show();});
+
+			$("#FL").on("click", function() {$(".card").hide();$("#FL-card").show();});
+
+			$("#PA").on("click", function() {$(".card").hide();$("#PA-card").show();});
+
+			$("#VA").on("click", function() {$(".card").hide();$("#VA-card").show();});
+
+			$("#SC").on("click", function() {$(".card").hide();$("#SC-card").show();});
+
+			$("#GA").on("click", function() {$(".card").hide();$("#GA-card").show();});
+
+			$("#MI").on("click", function() {$(".card").hide();$("#MI-card").show();});
+
+			$("#OH").on("click", function() {$(".card").hide();$("#OH-card").show();});
+
+			$("#WV").on("click", function() {$(".card").hide();$("#WV-card").show();});
+
+			$("#NC").on("click", function() {$(".card").hide();$("#NC-card").show();});
+
+			$("#AL").on("click", function() {$(".card").hide();$("#AL-card").show();});
+
+			$("#WI").on("click", function() {$(".card").hide();$("#WI-card").show();});
+
+			$("#IL").on("click", function() {$(".card").hide();$("#IL-card").show();});
+
+			$("#IN").on("click", function() {$(".card").hide();$("#IN-card").show();});
+
+			$("#KY").on("click", function() {$(".card").hide();$("#KY-card").show();});
+
+			$("#TN").on("click", function() {$(".card").hide();$("#TN-card").show();});
+
+			$("#MS").on("click", function() {$(".card").hide();$("#MS-card").show();});
+
+			$("#MN").on("click", function() {$(".card").hide();$("#MN-card").show();});
+
+			$("#IA").on("click", function() {$(".card").hide();$("#IA-card").show();});
+
+			$("#MO").on("click", function() {$(".card").hide();$("#MO-card").show();});
+
+			$("#AR").on("click", function() {$(".card").hide();$("#AR-card").show();});
+
+			$("#LA").on("click", function() {$(".card").hide();$("#LA-card").show();});
+
+			$("#ND").on("click", function() {$(".card").hide();$("#ND-card").show();});
+
+			$("#SD").on("click", function() {$(".card").hide();$("#SD-card").show();});
+
+			$("#NE").on("click", function() {$(".card").hide();$("#NE-card").show();});
+
+			$("#KS").on("click", function() {$(".card").hide();$("#KS-card").show();});
+
+			$("#OK").on("click", function() {$(".card").hide();$("#OK-card").show();});
+
+			$("#TX").on("click", function() {$(".card").hide();$("#TX-card").show();});
+
+			$("#MT").on("click", function() {$(".card").hide();$("#MT-card").show();});
+
+			$("#WY").on("click", function() {$(".card").hide();$("#WY-card").show();});
+
+			$("#CO").on("click", function() {$(".card").hide();$("#CO-card").show();});
+
+			$("#NM").on("click", function() {$(".card").hide();$("#NM-card").show();});
+
+			$("#ID").on("click", function() {$(".card").hide();$("#ID-card").show();});
+
+			$("#NV").on("click", function() {$(".card").hide();$("#NV-card").show();});
+
+			$("#UT").on("click", function() {$(".card").hide();$("#UT-card").show();});
+
+			$("#AZ").on("click", function() {$(".card").hide();$("#AZ-card").show();});
+
+			$("#WA").on("click", function() {$(".card").hide();$("#WA-card").show();});
+
+			$("#OR").on("click", function() {$(".card").hide();$("#OR-card").show();});
+
+			$("#CA").on("click", function() {$(".card").hide();$("#CA-card").show();});
+
+			$("#HI").on("click", function() {$(".card").hide();$("#HI-card").show();});
+
+			$("#AK").on("click", function() {$(".card").hide();$("#AK-card").show();});
+
+		});
+
+
